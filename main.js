47721204263d1330e6898844fa64c9adf56ae28b
sazidzid1 <157969523+sazidzid1@users.noreply.github.com>
2024-01-28 19:34:36 +0530

    Update main.js

--- main.js
+++ main.js
@@ -243,7 +243,7 @@ var event_data = {
         "year": 2024,
         "month": 1,
         "day": 28,
-        "cancelled": true
+        "cancelled": false
     },
     {
         "occasion": " Repeated Test Event ",
